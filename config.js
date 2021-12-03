1155ef70b5fc17a99976ee8df44991599a90c06e
elsahrul <sahrulramadhanh@gmail.com>
2021-12-04 03:26:52 +0700

    add token

--- config.js
+++ config.js
@@ -1,3 +1,26 @@
 var baseUrl = "https://localhost/admin-absensi/api";
 var locationHref = "/pwa-absensi/home.html";
-var indexHref = "/pwa-absensi/index.html"
+var indexHref = "/pwa-absensi/index.html";
+
+
+function verifikasi_token() {
+    var token = localStorage.getItem("token");
+    $.ajax({
+        url: `${baseUrl}/token/verifikasi_token`,
+        data: {
+            token: token
+        },
+        type: "POST",
+        dataType: "json",
+        success: function (response) {
+            if (response.status === "failed") {
+                localStorage.setItem('status_token', "failed");
+            } else {
+                localStorage.setItem('status_token', "success");
+            }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert(JSON.stringify(XMLHttpRequest));
+        }
+    });
+}
