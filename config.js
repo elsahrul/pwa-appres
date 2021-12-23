f195fdb839734c83b7144c5953bd7244930eed01
elsahrul <sahrulramadhanh@gmail.com>
2021-12-23 20:47:49 +0700

    push ke 2

--- config.js
+++ config.js
@@ -1,26 +1,26 @@
-var baseUrl = "https://192.168.137.1/admin-absensi/api";
+var baseUrl = "https://192.168.90.102/admin-absensi/api";
 var locationHref = "/pwa-absensi/home.html";
 var indexHref = "/pwa-absensi/index.html";
 
 
-function verifikasi_token() {
-    var token = localStorage.getItem("token");
-    $.ajax({
-        url: `${baseUrl}/token/verifikasi_token`,
-        data: {
-            token: token
-        },
-        type: "POST",
-        dataType: "json",
-        success: function (response) {
-            if (response.status === "failed") {
-                localStorage.setItem('status_token', "failed");
-            } else {
-                localStorage.setItem('status_token', "success");
-            }
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            alert(JSON.stringify(XMLHttpRequest));
-        }
-    });
-}
+// function verifikasi_token() {
+//     var token = localStorage.getItem("token");
+//     $.ajax({
+//         url: `${baseUrl}/token/verifikasi_token`,
+//         data: {
+//             token: token
+//         },
+//         type: "POST",
+//         dataType: "json",
+//         success: function (response) {
+//             if (response.status === "failed") {
+//                 localStorage.setItem('status_token', "failed");
+//             } else {
+//                 localStorage.setItem('status_token', "success");
+//             }
+//         },
+//         error: function (XMLHttpRequest, textStatus, errorThrown) {
+//             alert(JSON.stringify(XMLHttpRequest));
+//         }
+//     });
+// }
